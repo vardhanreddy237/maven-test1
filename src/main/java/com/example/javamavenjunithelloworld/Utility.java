7843b8aec51414320cfcc2c2a85a5be384e168b3
vardhanreddykallem <vardhanreddykallem@gmail.com>
2024-11-29 15:40:27 +0530

    VARDHANK237-Wrote the code for finding the area of the circle

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,6 @@
+public class Utility {
+    public static void areaOfCircle(double radius) {
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of Circle: " + area);
+    }
+}
